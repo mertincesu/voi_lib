105731f35736ac337eeda52a62076a1bf5c909ee
Mert Incesu <mert.incesu03@gmail.com>
2024-09-26 21:30:19 -0400

    updated version num

--- build/lib/voi_lib/__init__.py
+++ build/lib/voi_lib/__init__.py
@@ -1,5 +1,7 @@
-# Import the main function and classes from chatbot.py
-from .chatbot import create_voi_chatbot
+# __init__.py
 
-# Expose the create_voi_chatbot function at the top level of the package
-__all__ = ['create_voi_chatbot']
+from .chatbot import VoiAssistant
+
+# Make VoiAssistant accessible when importing the package
+__all__ = ['VoiAssistant']
+
